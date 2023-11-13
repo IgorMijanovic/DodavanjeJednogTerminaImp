778c11d43bb030098c851bf888724f54af869d47
IgorM213 <igor.mijanovic02@gmail.com>
2023-11-13 15:13:08 +0100

    dodavanje i brisanje za jedan termin

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        ZakazivanjeJedanImpl z = new ZakazivanjeJedanImpl();
+
+        z.dodajNoviTermin("s1", "13","15","01.10.2023", "Prof:aleksa", "Tip:predavanje");
+        z.dodajNoviTermin("s1", "13","15","02.10.2023", "Prof:igor");
+        z.dodajNoviTermin("s1", "13","15","03.10.2023", "Prof:kkkkkk");
+        z.brisanjeTermina("s1", "13","15","03.10.2023");
+
+
+
+        for (Termin termin: z.getRaspored()){
+            System.out.println(termin);
+        }
+    }
+}
